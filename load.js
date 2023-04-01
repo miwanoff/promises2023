109f78b20da41c26a636276ec2a50f2abcd1ac12
miwanoff <marina.iva.noffa@gmail.com>
2023-04-01 13:32:28 +0300

    Add insertBooks

--- load.js
+++ load.js
@@ -1,7 +1,31 @@
+//-----------------1----------------------
+
+// fetch("books.json")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+//-----------------2----------------------
+let booksBlock = document.querySelector("#books");
 fetch("books.json")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    console.log(data);
+    insertBooks(data);
   });
+function insertBooks(books) {
+  let str = `<div class="wrap">`;
+  for (let i = 0; i < books.length; i++) {
+    str += `<div class="bookWrap">`;  
+    str += `<h2>${books[i].title}</h2>`;
+    str += `<p>${books[i].author}</p>`;
+    str += `</div>`;
+  }
+  str += `</div>`;
+  booksBlock.innerHTML = str;
+}
+
+//-----------------3----------------------
