b22edd0a7057c37aa3afb814760f909c0bb2da77
miwanoff <marina.iva.noffa@gmail.com>
2023-04-01 13:33:45 +0300

    Add async loadBooks

--- load.js
+++ load.js
@@ -8,24 +8,31 @@
 //     console.log(data);
 //   });
 //-----------------2----------------------
-let booksBlock = document.querySelector("#books");
-fetch("books.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    insertBooks(data);
-  });
-function insertBooks(books) {
-  let str = `<div class="wrap">`;
-  for (let i = 0; i < books.length; i++) {
-    str += `<div class="bookWrap">`;  
-    str += `<h2>${books[i].title}</h2>`;
-    str += `<p>${books[i].author}</p>`;
-    str += `</div>`;
-  }
-  str += `</div>`;
-  booksBlock.innerHTML = str;
-}
+// let booksBlock = document.querySelector("#books");
+// fetch("books.json")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     insertBooks(data);
+//   });
+// function insertBooks(books) {
+//   let str = `<div class="wrap">`;
+//   for (let i = 0; i < books.length; i++) {
+//     str += `<div class="bookWrap">`;
+//     str += `<h2>${books[i].title}</h2>`;
+//     str += `<p>${books[i].author}</p>`;
+//     str += `</div>`;
+//   }
+//   str += `</div>`;
+//   booksBlock.innerHTML = str;
+// }
 
 //-----------------3----------------------
+async function loadBooks() {
+  let url = "books.json";
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+}
+loadBooks();
