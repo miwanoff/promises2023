0e0cbe9d0520e013575de407f88200f97b61b831
miwanoff <marina.iva.noffa@gmail.com>
2023-04-01 13:11:47 +0300

    Add catch and finally

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let promise = new Promise(function (resolve, reject) {
   setTimeout(() => resolve("Success!"), 1000); // задача выполнена успешно (через 1 секунду)
+ // setTimeout(() => reject(new Error("Error!")), 1000); // задача выполнена с ошибкой (через 1 секунду)
 });
 
 promise.then(
@@ -12,3 +13,7 @@ promise.then(
     alert(error);
   }
 );
+
+// promise.catch(() => alert("Ошибка")); 
+
+promise.finally(() => alert("Работа завершена"))
