4989b460983f8cb6da5e598c8b4f1fdaacb9fdb9
miwanoff <marina.iva.noffa@gmail.com>
2023-04-02 12:39:34 +0300

    Create weathet.js

---
+++ weather.js
@@ -0,0 +1,9 @@
+let city = "London";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=89ea4480f451ffd24b79617df1ba2bb0`;
+async function loadWeather() {
+    // let url = "books.json";
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+}
+loadWeather();
