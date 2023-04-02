028cb6ca468388df464f2c04ccb0d859e035bec1
miwanoff <marina.iva.noffa@gmail.com>
2023-04-02 12:51:14 +0300

    Add output

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 "use srtrict";
 const weatherBlock = document.querySelector("#weather");
-let city = "London";
+let city = "Kharkiv";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=89ea4480f451ffd24b79617df1ba2bb0`;
 async function loadWeather() {
   weatherBlock.innerHTML = `
@@ -9,6 +9,24 @@ async function loadWeather() {
   // let url = "books.json";
   let response = await fetch(url, { method: "GET" });
   let data = await response.json();
+  if (response.ok) {
+    getWeather(data);
+  } else {
+    weatherBlock.innerHTML = data.message;
+  }
+  //console.log(data);
+}
+
+function getWeather(data) {
   console.log(data);
+  const city = data.name;
+  const temp = data.main.temp;
+  const icon = data.weather[0].icon;
+  const template = `
+      <div id="city"><h2>${city}</h2></div>
+      <div id="temp">${temp}</div>
+      <div id="icon"><img src="https://api.openweathermap.org/img/w/${icon}.png" alt=""></div>
+      `;
+  weatherBlock.innerHTML = template;
 }
 loadWeather();
